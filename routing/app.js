a139e59ab6d686c284963e580630752e1a1c8ad9
Erfan Sharafi <erfansharafi60@gmail.com>
2025-01-24 23:40:17 +0330

    parameter routing part 2

--- routing/app.js
+++ routing/app.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 3000
 
 const users = [{ id: 1, name: "John" }, { id: 2, name: "Doe" }, { id: 3, name: "Erfan" }]
+const products = [{ id: 1, name: "product1" }, { id: 2, name: "product2" }, { id: 3, name: "product3" }]
+
 
 app.get('/', (req, res) => {
   res.send('hello world')
@@ -23,6 +25,20 @@ app.get('/users/:id', (req, res) => {
   } else {
     res.status(200).json({ statusCode: res.statusCode, data: { user } })
   }
+})
+
+app.get('/product/:id?', (req, res) => {
+  const { id } = req.params
+
+  let product = null
+
+  if (id) {
+    product = products.find(product => product.id === +id)
+    return res.status(200).json({ statusCode: res.statusCode, data: { product } })
+  }
+
+  res.status(200).json({ statusCode: res.statusCode, data: { products } })
+
 })
 
 
